b9f5b915c3ffc082da12cc27e0343a40f0349358
Jamal Issa <jkhissa@gmail.com>
2025-11-22 21:55:33 +0000

    config: Update Vite base path to '/jamal-issa-portfolio/'.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
     // If you are deploying to https://<USERNAME>.github.io/<REPO>/, set base to '/<REPO>/'.
-    base: '/',
+    base: '/jamal-issa-portfolio/',
 })
